a8cedcfff81f1280ef014c9fe1bd0c25f9d6315a
chrrhod <118434632+chrrhod@users.noreply.github.com>
2026-02-05 14:05:25 +0100

    Added mandelbrot skeleton

---
+++ mandelbrot.py
@@ -0,0 +1,18 @@
+"""
+Mandelbrot Set Generator
+Author : [ Your Name ]
+Course : Numerical Scientific Computing 2026
+"""
+def f(x):
+"""
+Example function .
+Parameters
+----------
+x : float
+Input value
+Returns
+-------
+float
+Output value
+"""
+# TODO
