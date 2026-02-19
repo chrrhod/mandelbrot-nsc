1341d82d9b0cee3385129d6a25d6193543df0295
chrrhod <118434632+chrrhod@users.noreply.github.com>
2026-02-19 10:35:18 +0100

    Added compute_mandelbrot function
    The compute_mandelbrot function takes a x and a y point along with the amount of points on each axis and the max iterations.
    
    It then calculates a grid and checks if each point in the grid is in the mandelbrot_set
    
    In the end it returns the mandelbrot set

--- mandelbrot.py
+++ mandelbrot.py
@@ -3,6 +3,8 @@ Mandelbrot Set Generator
 Author : Christian Rhod
 Course : Numerical Scientific Computing 2026
 """
+import numpy as np
+
 def mandelbrot_point(c, max_iter):
     z = 0
     for n in range(max_iter):
@@ -11,3 +13,13 @@ def mandelbrot_point(c, max_iter):
             return n
     return max_iter
 
+
+def compute_mandelbrot(xmin, xmax, ymin, ymax, width, height, max_iter):
+    x = np.linspace(xmin, xmax, width)
+    y = np.linspace(ymin, ymax, height)
+    mandelbrot_set = np.zeros((height, width), dtype=int)
+    for i in range(height):
+        for j in range(width):
+            c = complex(x[j], y[i])
+            mandelbrot_set[i, j] = mandelbrot_point(c, max_iter)
+    return mandelbrot_set
