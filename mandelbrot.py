fec6532d19880ecb5f848b9cebef3e1ac43e1d74
chrrhod <118434632+chrrhod@users.noreply.github.com>
2026-02-19 11:24:57 +0100

    Added a function to time other functions

--- mandelbrot.py
+++ mandelbrot.py
@@ -5,6 +5,7 @@ Course : Numerical Scientific Computing 2026
 """
 import numpy as np
 import matplotlib.pyplot as plt
+from time import time
 
 
 def mandelbrot_point(c, max_iter):
@@ -40,6 +41,13 @@ def view_mandelbrot(mandelbrot_set, xmin, xmax, ymin, ymax):
     #save the image
     plt.imsave('mandelbrot.png', mandelbrot_set, cmap='inferno')
 
+def time_function(func, *args):
+    start_time = time()
+    result = func(*args)
+    end_time = time()
+    print(f"Function {func.__name__} took {end_time - start_time:.2f} seconds")
+    return result
+
 if __name__ == "__main__":
     # Parameters for the Mandelbrot set
     xmin, xmax, ymin, ymax = -2.0, 1.0, -1.5, 1.5
@@ -47,8 +55,10 @@ if __name__ == "__main__":
     width, height = 1024, 1024
     max_iter = 100
 
-    # Compute the Mandelbrot set
-    mandelbrot_set = compute_mandelbrot(xmin, xmax, ymin, ymax, width, height, max_iter)
+
+    mandelbrot_set = time_function(compute_mandelbrot,xmin, xmax, ymin, ymax, width, height, max_iter)
+
+
 
     # Visualize the Mandelbrot set
     view_mandelbrot(mandelbrot_set, xmin, xmax, ymin, ymax)
